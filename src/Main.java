489a12a9bc8ce28aac4bf7dfebb12e7749204121
ykliek <www.yura.klek@gmail.com>
2023-10-09 18:21:50 +0300

    The END!

--- src/Main.java
+++ src/Main.java
@@ -13,15 +13,26 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * This represents main login of the program.
+ */
 public class Main {
+
+	private static final Scanner scanner = new Scanner(System.in);;
+	/**
+	 * Main method of the program.
+	 * @param args command line arguments
+	 * @throws ParseException if date is not in the correct format
+	 */
 	public static void main(String[] args) throws ParseException {
-		Scanner scanner = new Scanner(System.in);
 		TriviaNightCollection triviaNightCollection = new TriviaNightCollection();
 
 		System.out.println("Welcome to the Trivia Night Database!");
 		int choice;
+		String pathToFile;
 
 		do {
 			System.out.println("Choose an option to proceed:");
@@ -29,43 +40,62 @@ public class Main {
 			System.out.println("2 - Remove a Trivia Night event");
 			System.out.println("3 - Print all Trivia Night events");
 			System.out.println("4 - Sort Trivia Night events by number of participants");
-			System.out.println("5 - Search Trivia Night events by keyword");
-			System.out.println("6 - Save events to file");
-			System.out.println("7 - Load events from file");
+			System.out.println("5 - Sort Trivia Night events by date");
+			System.out.println("6 - Search Trivia Night events by keyword");
+			System.out.println("7 - Search Trivia Night events by year");
+			System.out.println("8 - Save events to file");
+			System.out.println("9 - Load events from file");
 			System.out.println("0 - Exit the program");
 			System.out.print("Enter your choice: ");
 
-			choice = scanner.nextInt();
+			choice = getUserInt();
 			scanner.nextLine(); // Consume the newline character
 
 			switch (choice) {
 				case 1:
-					TriviaNight newEvent = createTriviaNightEvent(scanner);
+					TriviaNight newEvent = createTriviaNightEvent();
 					triviaNightCollection.add(newEvent);
-					System.out.println("Trivia Night event added to the collection.");
+					System.out.println(
+							"Trivia Night event added to the collection.");
 					break;
 				case 2:
-					System.out.print("Enter the index of the event to remove: ");
-					int indexToRemove = scanner.nextInt();
-					triviaNightCollection.remove(indexToRemove);
+					System.out.print(
+							"Enter the index of the event to remove: ");
+					int indexToRemove = getUserInt();
+					triviaNightCollection.remove(indexToRemove - 1);
 					break;
 				case 3:
 					triviaNightCollection.printAll();
 					break;
 				case 4:
-					triviaNightCollection.sort();
-					System.out.println("Trivia Night events sorted by number of participants.");
+					triviaNightCollection.sortByNumberOfParticipants();
+					System.out.println(
+							"Trivia Night events sorted by number of " +
+									"participants.");
 					break;
 				case 5:
-					System.out.print("Enter a keyword to search for events: ");
-					String keyword = scanner.nextLine();
-					triviaNightCollection.search(keyword);
+					triviaNightCollection.sortByDate();
+					System.out.println("Trivia Night events sorted by date.");
 					break;
 				case 6:
-					triviaNightCollection.saveToFile();
+					System.out.print("Enter a keyword to search for events: ");
+					String keyword = scanner.nextLine();
+					triviaNightCollection.searchByDescription(keyword);
 					break;
 				case 7:
-					triviaNightCollection.readFromFile();
+					System.out.print("Enter a year to search for events: ");
+					int year = getUserInt();
+					triviaNightCollection.searchByDateYear(year);
+					break;
+				case 8:
+					System.out.print("Enter path to file: ");
+					pathToFile = scanner.nextLine();
+					triviaNightCollection.saveToFile(pathToFile);
+					break;
+				case 9:
+					System.out.print("Enter path to file: ");
+					pathToFile = scanner.nextLine();
+					triviaNightCollection.readFromFile(pathToFile);
 					break;
 				case 0:
 					System.out.println("Exiting the program. Goodbye!");
@@ -78,14 +108,19 @@ public class Main {
 		scanner.close();
 	}
 
-	private static TriviaNight createTriviaNightEvent(Scanner scanner) throws ParseException {
+	/**
+	 * Creates a new Trivia Night event with user input.
+	 * @return TriviaNight object
+	 * @throws ParseException if date is not in the correct format
+	 */
+	private static TriviaNight createTriviaNightEvent() throws ParseException {
 		System.out.println("Adding a new Trivia Night event to the collection:");
 		System.out.print("Event Name: ");
 		String eventName = scanner.nextLine();
 		System.out.print("Event Date (dd-MM-yyyy): ");
-		Date eventDate = new SimpleDateFormat("dd-MM-yyyy").parse(scanner.nextLine());
+		Date eventDate = getUserDate();
 		System.out.print("Number of Participants: ");
-		int numberOfParticipants = scanner.nextInt();
+		int numberOfParticipants = getUserInt();
 		scanner.nextLine(); // Consume the newline character
 		System.out.print("Main Theme: ");
 		String mainTheme = scanner.nextLine();
@@ -100,5 +135,29 @@ public class Main {
 
 		return new TriviaNight(eventName, eventDate, numberOfParticipants, mainTheme,
 				registerUrl, chatUrl, eventLocation, descriptionFilePath);
+	}
+
+	private static Date getUserDate() {
+		while(true) {
+			try {
+				return new SimpleDateFormat(
+						"dd-MM-yyyy").parse(scanner.nextLine());
+			} catch (ParseException e) {
+				System.out.println(
+						"\u001B[31mIt is not a date. Try again: \u001B[0m");
+			}
+		}
+	}
+
+	private static int getUserInt() {
+		while (true) {
+			try {
+				return scanner.nextInt();
+			} catch (InputMismatchException e) {
+				scanner.nextLine();
+				System.out.println(
+						"\u001B[31mIt is not a number. Try again: \u001B[0m");
+			}
+		}
 	}
 }
--- src/TriviaNight.java
+++ src/TriviaNight.java
@@ -1,11 +1,11 @@
 import java.io.*;
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
+/**
+ * This class represents a Trivia Night event.
+ */
 public class TriviaNight {
 	private String eventName;
 	private Date eventDate;
@@ -16,8 +16,10 @@ public class TriviaNight {
 	private String eventLocation;
 	private String eventDescription;
 
-	public TriviaNight(String eventName, Date eventDate, int numberOfParticipants, String mainTheme,
-	                   String registerUrl, String chatUrl, String eventLocation, String eventDescription) {
+	public TriviaNight(
+			String eventName, Date eventDate, int numberOfParticipants,
+			String mainTheme, String registerUrl, String chatUrl,
+			String eventLocation, String eventDescription) {
 		this.eventName = eventName;
 		this.eventDate = eventDate;
 		this.numberOfParticipants = numberOfParticipants;
@@ -42,7 +44,8 @@ public class TriviaNight {
 	}
 
 	public void setEventDate(String eventDate) throws ParseException {
-		this.eventDate = new SimpleDateFormat("dd-MM-yyyy").parse(eventDate);
+		this.eventDate = new SimpleDateFormat(
+				"dd-MM-yyyy").parse(eventDate);
 	}
 
 	public int getNumberOfParticipants() {
@@ -95,16 +98,14 @@ public class TriviaNight {
 
 	// Additional methods
 	public String getDescription() {
-		StringBuilder descriptionBuilder = new StringBuilder();
-		descriptionBuilder.append("Event Name: ").append(eventName).append("\n");
-		descriptionBuilder.append("Event Date: ").append(eventDate).append("\n");
-		descriptionBuilder.append("Number of Participants: ").append(numberOfParticipants).append("\n");
-		descriptionBuilder.append("Main Theme: ").append(mainTheme).append("\n");
-		descriptionBuilder.append("Register URL: ").append(registerUrl).append("\n");
-		descriptionBuilder.append("Chat URL: ").append(chatUrl).append("\n");
-		descriptionBuilder.append("Event Location: ").append(eventLocation).append("\n");
-		descriptionBuilder.append("Event Description: ").append(eventDescription).append("\n");
-		return descriptionBuilder.toString();
+		return "Event Name: " + eventName + "\n" +
+				"Event Date: " + eventDate + "\n" +
+				"Number of Participants: " + numberOfParticipants + "\n" +
+				"Main Theme: " + mainTheme + "\n" +
+				"Register URL: " + registerUrl + "\n" +
+				"Chat URL: " + chatUrl + "\n" +
+				"Event Location: " + eventLocation + "\n" +
+				"Event Description: " + eventDescription + "\n";
 	}
 
 	public String getTitle() {
@@ -112,71 +113,199 @@ public class TriviaNight {
 	}
 }
 
-class TriviaNightCollection {
-	private List<TriviaNight> events = new ArrayList<>();
 
-	public void add(TriviaNight newEvent) {
-		events.add(newEvent);
+/**
+ * This class represents a collection of Trivia Night events.
+ */
+class TriviaNightCollection {
+	private TriviaNight[] events;
+	private int count;
+
+	public TriviaNightCollection() {
+		events = new TriviaNight[10]; // Initial capacity of 10
+		count = 0;
 	}
 
-	public void remove(int i) {
-		if (i >= 0 && i < events.size()) {
-			events.remove(i);
+	public void add(TriviaNight newEvent) {
+		if (count == events.length) {
+			// Expand the array if needed
+			events = Arrays.copyOf(events, count * 2);
+		}
+		events[count++] = newEvent;
+	}
+
+	public void remove(int index) {
+		if (index >= 0 && index < count) {
+			System.arraycopy(
+					events, index + 1, events, index,
+					count - index - 1);
+			events[count - 1] = null; // Clear the last element
+			count--;
 		}
 	}
 
-	public void printOne(int i) {
-		if (i >= 0 && i < events.size()) {
-			System.out.println(events.get(i).getDescription());
+	public void printOne(int index) {
+		if (index >= 0 && index < count) {
+			System.out.println(events[index].getDescription());
 		} else {
 			System.out.println("Invalid index.");
 		}
 	}
 
 	public void printAll() {
-		for (int i = 0; i < events.size(); i++) {
+		for (int i = 0; i < count; i++) {
 			System.out.println("Event #" + (i + 1));
-			System.out.println(events.get(i).getDescription());
+			System.out.println(events[i].getDescription());
 		}
 	}
 
-	public void printList() {
-		for (int i = 0; i < events.size(); i++) {
-			System.out.println((i + 1) + ". " + events.get(i).getTitle());
-		}
+	public void sortByNumberOfParticipants() {
+		Arrays.sort(
+				events, 0, count,
+				Comparator.comparingInt(TriviaNight::getNumberOfParticipants));
 	}
 
-	public void sort() {
-		Collections.sort(events, (event1, event2) ->
-				Integer.compare(event1.getNumberOfParticipants(), event2.getNumberOfParticipants()));
+	public void sortByDate() {
+		Arrays.sort(
+				events, 0, count,
+				Comparator.comparing(TriviaNight::getEventDate));
 	}
 
-	public void search(String phrase) {
-		for (int i = 0; i < events.size(); i++) {
-			if (events.get(i).getDescription().contains(phrase)) {
+	public void searchByDescription(String phrase) {
+		for (int i = 0; i < count; i++) {
+			if (events[i].getDescription().contains(phrase)) {
 				System.out.println("Event #" + (i + 1));
-				System.out.println(events.get(i).getDescription());
+				System.out.println(events[i].getDescription());
 			}
 		}
 	}
 
-	// Save all events to a specific file (e.g., "events.txt")
-	public void saveToFile() {
-		try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("events.txt"))) {
-			oos.writeObject(events);
-			System.out.println("Events saved to file.");
-		} catch (IOException e) {
-			e.printStackTrace();
+	public void searchByDateYear(int searchYear) {
+		Calendar calendar = Calendar.getInstance();
+		for (int i = 0; i < count; i++) {
+			calendar.setTime(events[i].getEventDate());
+			int eventYear = calendar.get(Calendar.YEAR);
+			if (eventYear == searchYear) {
+				System.out.println("Event #" + (i + 1));
+				System.out.println(events[i].getDescription());
+			}
 		}
 	}
 
-	// Read all events from the specific file (e.g., "events.txt")
-	public void readFromFile() {
-		try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("events.txt"))) {
-			events = (List<TriviaNight>) ois.readObject();
-			System.out.println("Events loaded from file.");
-		} catch (IOException | ClassNotFoundException e) {
-			e.printStackTrace();
+	private String getAll() {
+		StringBuilder result = new StringBuilder();
+		for (int i = 0; i < count; i++) {
+			result.append("Event #").append(i + 1).append("\n");
+			result.append("Event Name: ");
+			result.append(events[i].getEventName()).append("\n");
+			result.append("Event Date: ");
+			result.append(events[i].getEventDate()).append("\n");
+			result.append("Number of participants: ");
+			result.append(events[i].getNumberOfParticipants()).append("\n");
+			result.append("Main Theme: ");
+			result.append(events[i].getMainTheme()).append("\n");
+			result.append("Register URL: ");
+			result.append(events[i].getRegisterUrl()).append("\n");
+			result.append("Chat URL: ");
+			result.append(events[i].getChatUrl()).append("\n");
+			result.append("Event Location: ");
+			result.append(events[i].getEventLocation()).append("\n");
+			result.append("Event Description: ");
+			result.append(events[i].getEventDescription()).append("\n");
+			result.append("\n");
+		}
+		return result.toString();
+	}
+
+	public void saveToFile(String path) {
+		try {
+			FileWriter fileWriter = new FileWriter(path);
+			BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+			bufferedWriter.write(getAll());
+
+			bufferedWriter.close();
+			System.out.println(
+					"Content has been written to the file successfully.");
+		} catch (IOException e) {
+			System.err.println("An error occurred: " + e.getMessage());
+		}
+	}
+
+	public void readFromFile(String path) {
+		try (FileReader fileReader = new FileReader(path);
+		     BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+			String eventName = "";
+			Date eventDate = null;
+			int numberOfParticipants = 0;
+			String mainTheme = "";
+			String registerUrl = "";
+			String chatUrl = "";
+			String eventLocation = "";
+			String eventDescription = "";
+
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				String value = "";
+				if (line.contains(":") && line.indexOf(":") + 2 < line.length()) {
+					value = line.substring(line.indexOf(":") + 2);
+				}
+				if (line.contains("Event Name:")) {
+					eventName = value;
+				}
+				if (line.contains("Event Date:")) {
+					try {
+						SimpleDateFormat dateFormat = new SimpleDateFormat(
+								"E MMM dd HH:mm:ss z yyyy", Locale.US);
+						eventDate = dateFormat.parse(value);
+					} catch (ParseException e) {
+						// Handle date parsing error gracefully, log the error, and continue
+						System.err.println("Error parsing date: " + value);
+					}
+				}
+				if (line.contains("Number of participants")) {
+					try {
+						numberOfParticipants = Integer.parseInt(value);
+					} catch (NumberFormatException e) {
+						// Handle number parsing error gracefully, log the error, and continue
+						System.err.println(
+								"Error parsing number of participants: " + value
+						);
+					}
+				}
+				if (line.contains("Main Theme")) {
+					mainTheme = value;
+				}
+				if (line.contains("Register URL")) {
+					registerUrl = value;
+				}
+				if (line.contains("Chat URL")) {
+					chatUrl = value;
+				}
+				if (line.contains("Event Location")) {
+					eventLocation = value;
+				}
+				if (line.contains("Event Description")) {
+					eventDescription = value;
+				}
+				if (line.trim().isEmpty()) {
+					add(new TriviaNight(eventName, eventDate,
+							numberOfParticipants, mainTheme, registerUrl,
+							chatUrl, eventLocation, eventDescription));
+					// Reset fields for the next event
+					eventName = "";
+					eventDate = null;
+					numberOfParticipants = 0;
+					mainTheme = "";
+					registerUrl = "";
+					chatUrl = "";
+					eventLocation = "";
+					eventDescription = "";
+				}
+			}
+		} catch (IOException e) {
+			System.err.println("An error occurred: " + e.getMessage());
 		}
 	}
 }
